f486f371f23453a8da22affadf2689e1c3f1307e
Mike Lynch <nycynik@gmail.com>
2025-08-14 14:54:59 -0700

    Update commands-create.sh

--- lib/commands-create.sh
+++ lib/commands-create.sh
@@ -21,8 +21,8 @@ cmd_create() {
         read -rp "Enter ticket description: " description
     fi
 
-    acli create workitem --project "$(get_config_value project)" --type "$(get_config_value type)" --summary "$title" --description "$description" --assignee "@me"
-        # acli jira issue create --project "PROJECT_KEY" --type "Task" --summary "$1" --description "$2"
+    acli jira workitem create --project "$(get_config_value project)" --type "$(get_config_value type)" --summary "$title" --description "$description" --assignee "@me"
+    # ✓ Work item CCS-7 created: https://theinfluence.atlassian.net/browse/CCS-7        # acli jira issue create --project "PROJECT_KEY" --type "Task" --summary "$1" --description "$2"
         # gh issue create --title "New ticket title" --body "Detailed description of the new ticket"
     echo "Creating ticket with title: $title and description: $description"
     print_status_message success "New ticket created successfully."
